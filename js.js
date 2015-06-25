4a3369c876c7fcfa1a3131e2afcfdfdfab46cd20
Andy Bateman <andy@andybateman.com>
2015-06-25 14:05:33 +1200

    Colour reporting now up and running

--- js.js
+++ js.js
@@ -1,7 +1,26 @@
+var colours = ['Red', 'Green', 'Blue', 'Purple'];
+
+function readyForCount() {
+	var ul = document.getElementById("countResults");
+	ul.innerHTML = '';
+	
+	colours.forEach(function(colour) {
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(colour + ": " + countColours(colour)));
+		ul.appendChild(li);
+	});
+}
 
 function countColours(searchTerm) {
 	searchExpression = new RegExp(searchTerm, 'gi');
-	return document.getElementById("colourText").value.match(searchExpression).length;
+
+	if (document.getElementById("colourText").value.match(searchExpression)) {
+		result = document.getElementById("colourText").value.match(searchExpression).length;
+	} else {
+		result = 0;
+	}
+	;
+	return result;
 }
 
 function getParameter(name) {
